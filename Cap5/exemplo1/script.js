071b05dae280304377ce0837c6ecac3b81b5db1e
PedroLopes7K <pedrohenri3601@gmail.com>
2022-04-03 16:03:58 -0300

    Update 03/04

---
+++ Cap5/exemplo1/script.js
@@ -0,0 +1,15 @@
+const form = document.querySelector('form')
+const numero = document.getElementById('numero')
+const resposta = document.getElementById('resposta')
+
+form.addEventListener('submit', e => {
+  e.preventDefault()
+  let valor = numero.value
+  let res = ''
+  for (let i = 1; i <= 10; i++) {
+    let resultado = valor * i
+    // res += `${valor} x ${i} = ${resultado}\n`
+    resposta.innerText += `${valor} x ${i} = ${resultado}\n`
+  }
+  // resposta.innerText = res
+})
