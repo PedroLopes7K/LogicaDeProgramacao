8af6782c3f95da461f42fa9a9a3a0cea938170e5
PedroLopes7K <pedrohenri3601@gmail.com>
2022-04-10 12:52:04 -0300

    Update 10/04

---
+++ Cap6/exercicio.js
@@ -0,0 +1,44 @@
+// RETORNAR CANDIDATOS APROVADOS (REQUISITOS: MAIOR DE IDADE, CONHECIMENTOS: REACT, NODE, JAVASCRIPT)
+const candidatos = [
+  {
+    nome: 'Pedro',
+    idade: 19,
+    conhecimentos: ['ReactJS', 'JavaScript', 'HTML', 'CSS', 'Node']
+  },
+  {
+    nome: 'Lucas',
+    idade: 29,
+    conhecimentos: ['VueJS', 'JavaScript', 'Python', 'C++', 'ReactJS']
+  },
+  {
+    nome: 'Juliana',
+    idade: 22,
+    conhecimentos: ['PHP', 'Laravel', 'SQL', 'MySql']
+  },
+  {
+    nome: 'Bruno',
+    idade: 17,
+    conhecimentos: ['ReactJS', 'JavaScript', 'HTML', 'Node', 'CSS']
+  },
+  {
+    nome: 'Mariana',
+    idade: 21,
+    conhecimentos: ['Angular', 'JavaScript', 'HTML', 'ReactJS', 'CSS']
+  }
+]
+function FiltraCandidatos(candidato) {
+  let aprovados = candidato.filter(
+    aprovado =>
+      aprovado.idade >= 18 &&
+      aprovado.conhecimentos.includes('JavaScript') &&
+      aprovado.conhecimentos.includes('ReactJS') &&
+      aprovado.conhecimentos.includes('Node')
+  )
+  let candidatosAprovados = []
+
+  for (let aprovado of aprovados) {
+    candidatosAprovados.push(aprovado.nome)
+  }
+  console.log(`Candidato(s) aprovado(s): ${candidatosAprovados}`)
+}
+FiltraCandidatos(candidatos)
